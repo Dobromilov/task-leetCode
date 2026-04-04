bad91f6022b9b2f888420adb110d707830b1b1a3
Dobromilov <dobromilovprojects@gmail.com>
2026-04-04 17:53:03 +0700

    Time: 1 ms (68.72%) | Memory: 8.5 MB (6.10%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/top-k-frequent-elements.go
@@ -1,17 +1,25 @@
 func topKFrequent(nums []int, k int) []int {
-    m := make(map[int]int)
-    for _, v := range nums{
-        m[v]++
-    }
+    count := make(map[int]int)
+	bucket := make([][]int, len(nums) + 1) 
 
-    res := make([]int, 0, len(m))
-    for num := range m {
-        res = append(res, num)
-    }
+	for _, v := range nums {
+		count[v]++
+	}
 
-    sort.Slice(res, func(i, j int) bool {
-        return m[res[i]] > m[res[j]]
-    })
+	for k, v := range count {
+		bucket[v] = append(bucket[v], k)
+	}
 
-    return res[:k]
+    res := []int {}
+
+	for i := len(bucket) - 1; i > 0; i-- {
+		for _, num := range bucket[i] {
+			res = append(res, num)
+			if len(res) == k{
+				return res
+			}
+		}
+	}
+
+	return res
 }
