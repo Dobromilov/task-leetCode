0327f7438135605d959c5e30dce9955da7e38ae7
Dobromilov <dobromilovprojects@gmail.com>
2026-04-04 17:04:07 +0700

    Time: 12 ms (66.67%) | Memory: 10.9 MB (75.00%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.go
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.go
@@ -1,25 +1,14 @@
 func decodeCiphertext(encodedText string, rows int) string {
-    if rows == 0 {
-        return ""
-    }
     cols := len(encodedText) / rows
-    matrix := make([][]rune, rows)
-    for i := 0; i < rows; i++ {
-        matrix[i] = make([]rune, cols)
-    }
-    count := 0
-    for i := 0; i < rows; i++ {
-        for j := 0; j < cols; j++ {
-            matrix[i][j] = rune(encodedText[count])
-            count++
-        }
-    }
-
-    var res []rune
+    result := make([]byte, 0)
     for j := 0; j < cols; j++ {
-        for i := 0; i < rows && i+j < cols; i++ {
-            res = append(res, matrix[i][i+j])
+        row, col := 0, j
+        for row < rows && col < cols {
+            index := row*cols + col
+            result = append(result, encodedText[index])
+            row++
+            col++
         }
     }
-    return strings.TrimRight(string(res), " ")
+    return strings.TrimRight(string(result), " ")
 }
