b41a2e7513d02693684a3b60c958183655e755e9
Dobromilov <dobromilovprojects@gmail.com>
2026-04-06 23:11:51 +0700

    Time: 0 ms (100.00%) | Memory: 8.5 MB (78.18%) - LeetSync

--- 792-binary-search/binary-search.go
+++ 792-binary-search/binary-search.go
@@ -1,7 +1,16 @@
 func search(nums []int, target int) int {
-    idx, found := slices.BinarySearch(nums, target)
-    if found {
-        return idx
+    l:=0
+    r:=len(nums)-1
+    for l <= r{
+        mid := (r+l)/2
+        if nums[mid] == target{
+            return mid
+        }
+        if nums[mid]<target{
+            l = mid+1
+        } else {
+            r = mid-1
+        }
     }
     return -1
 }
