7944695dd29dfbf6d1e85366f2e8f397403ba0d9
Dobromilov <dobromilovprojects@gmail.com>
2026-04-29 16:11:01 +0700

    Time: 5 ms (66.94%) | Memory: 8 MB (6.07%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+    m := make(map[*ListNode]bool)
+
+    for head != nil{
+        if m[head] == true{
+            return true
+        }
+        m[head] = true
+        head = head.Next
+    }
+
+    return false
+}
