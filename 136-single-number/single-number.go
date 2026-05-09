d3ea9baf17a26c2a81350f83ecc10ecbcd8df086
Dobromilov <dobromilovprojects@gmail.com>
2026-05-09 16:54:05 +0700

    Time: 1 ms (30.63%) | Memory: 8.2 MB (30.89%) - LeetSync

--- 136-single-number/single-number.go
+++ 136-single-number/single-number.go
@@ -1,7 +1,7 @@
 func singleNumber(nums []int) int {
     m := make(map[int]int, len(nums)/2+1)
-    for i := 0; i<len(nums) ; i++{
-        m[nums[i]]++
+    for _, n := range nums {
+        m[n] += 1
     }
     res:=0
     for _, n := range nums{
