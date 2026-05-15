cb24fd87a6132e3409374789e13fc64f01a1d68d
Dobromilov <dobromilovprojects@gmail.com>
2026-05-15 22:19:52 +0700

    Time: 0 ms (100.00%) | Memory: 4.4 MB (4.12%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -0,0 +1,11 @@
+func findMin(nums []int) int {
+    mn:=nums[0]
+    temp:=nums[0]
+    for _,val := range nums{
+        if val < mn && val<temp{
+            mn = val
+            break
+        }
+    }
+    return mn
+}
